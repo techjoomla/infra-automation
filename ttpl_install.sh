2e7bcd90501be97e6d9fd2f5f8db5bf8cba097c6
Manoj L <manoj_l@techjoomla.com>
2021-03-29 10:54:13 +0530

    Issue #150912: Allow installing all PHP versions

--- ttpl_install.sh
+++ ttpl_install.sh
@@ -4,10 +4,10 @@ read -p "Enter non-admin username (eg: ttpl1) : "  non_admin_username
 
 php8_name="${non_admin_username}-php8.local"
 php74_name="${non_admin_username}-php74.local"
-#php73_name="${non_admin_username}-php73.local"
-#php72_name="${non_admin_username}-php72.local"
-#php71_name="${non_admin_username}-php71.local"
-#php7_name="${non_admin_username}-php7.local"
+php73_name="${non_admin_username}-php73.local"
+php72_name="${non_admin_username}-php72.local"
+php71_name="${non_admin_username}-php71.local"
+php7_name="${non_admin_username}-php7.local"
 php5_name="${non_admin_username}-php5.local"
 non_admin_home_dir="/home/${non_admin_username}"
 
@@ -21,34 +21,34 @@ if [[ -r /etc/lsb-release ]]; then
 	if [[ ( $ID == "ubuntu" ) || ( $DISTRIB_ID == "Ubuntu" ) ]]; then
 		echo "Running Ubuntu $UBUNTU_VERSION_NAME $DISTRIB_CODENAME"
 
-	sudo apt-add-repository -y ppa:ansible/ansible
-	sudo apt-get update
-	sudo apt-get -y install ansible
+		sudo apt-add-repository -y ppa:ansible/ansible
+		sudo apt-get update
+		sudo apt-get -y install ansible
 
-	sudo wget -q https://github.com/techjoomla/infra-automation/archive/master.zip -O /tmp/master.zip
-	sudo unzip -oq /tmp/master.zip -d /tmp
+		sudo wget -q https://github.com/techjoomla/infra-automation/archive/master.zip -O /tmp/master.zip
+		sudo unzip -oq /tmp/master.zip -d /tmp
 
-	# Env. setup
-	sudo ansible-playbook -i "hosts," -c local /tmp/infra-automation-master/environment-setup.yml --skip-tags "createuser,ansible,aptupdate,python" --extra-vars="server_runs_as=$non_admin_username" #-vvv
+		# Env. setup
+		sudo ansible-playbook -i "hosts," -c local /tmp/infra-automation-master/environment-setup.yml --skip-tags "createuser,ansible,aptupdate,python" --extra-vars="server_runs_as=$non_admin_username" #-vvv
 
-	# Create PHP env.s
-	# Uncomment #vvv below to debug ansible
-	sudo ansible-playbook -i "localhost," -c local /tmp/infra-automation-master/create-site.yml --extra-vars="which_host=localhost site_domain=$php5_name site_id=php5 php_install_version=5.6 server_runs_as=$non_admin_username server_runs_as_group=$non_admin_username vhost_ssl=1 site_ssl=1 ssl_selfsigned=1" #-vvv
+		# Create PHP env.s
+		# Uncomment #vvv below to debug ansible
+		sudo ansible-playbook -i "localhost," -c local /tmp/infra-automation-master/create-site.yml --extra-vars="which_host=localhost site_domain=$php5_name site_id=php5 php_install_version=5.6 server_runs_as=$non_admin_username server_runs_as_group=$non_admin_username vhost_ssl=1 site_ssl=1 ssl_selfsigned=1" #-vvv
 
-	#sudo ansible-playbook -i "localhost," -c local /tmp/infra-automation-master/create-site.yml --extra-vars="which_host=localhost site_domain=$php7_name site_id=php7 php_install_version=7.0 server_runs_as=$non_admin_username server_runs_as_group=$non_admin_username" #-vvv
+		sudo ansible-playbook -i "localhost," -c local /tmp/infra-automation-master/create-site.yml --extra-vars="which_host=localhost site_domain=$php7_name site_id=php7 php_install_version=7.0 server_runs_as=$non_admin_username server_runs_as_group=$non_admin_username" #-vvv
 
-	#sudo ansible-playbook -i "localhost," -c local /tmp/infra-automation-master/create-site.yml --extra-vars="which_host=localhost site_domain=$php71_name site_id=php71 php_install_version=7.1 server_runs_as=$non_admin_username server_runs_as_group=$non_admin_username" #-vvv
+		sudo ansible-playbook -i "localhost," -c local /tmp/infra-automation-master/create-site.yml --extra-vars="which_host=localhost site_domain=$php71_name site_id=php71 php_install_version=7.1 server_runs_as=$non_admin_username server_runs_as_group=$non_admin_username" #-vvv
 
-	#sudo ansible-playbook -i "localhost," -c local /tmp/infra-automation-master/create-site.yml --extra-vars="which_host=localhost site_domain=$php72_name site_id=php72 php_install_version=7.2 server_runs_as=$non_admin_username server_runs_as_group=$non_admin_username" #-vvv
+		sudo ansible-playbook -i "localhost," -c local /tmp/infra-automation-master/create-site.yml --extra-vars="which_host=localhost site_domain=$php72_name site_id=php72 php_install_version=7.2 server_runs_as=$non_admin_username server_runs_as_group=$non_admin_username" #-vvv
 
-	#sudo ansible-playbook -i "localhost," -c local /tmp/infra-automation-master/create-site.yml --extra-vars="which_host=localhost site_domain=$php73_name site_id=php73 php_install_version=7.3 server_runs_as=$non_admin_username server_runs_as_group=$non_admin_username" #-vvv
+		sudo ansible-playbook -i "localhost," -c local /tmp/infra-automation-master/create-site.yml --extra-vars="which_host=localhost site_domain=$php73_name site_id=php73 php_install_version=7.3 server_runs_as=$non_admin_username server_runs_as_group=$non_admin_username" #-vvv
 
-	sudo ansible-playbook -i "localhost," -c local /tmp/infra-automation-master/create-site.yml --extra-vars="which_host=localhost site_domain=$php74_name site_id=php74 php_install_version=7.4 server_runs_as=$non_admin_username server_runs_as_group=$non_admin_username" #-vvv
+		sudo ansible-playbook -i "localhost," -c local /tmp/infra-automation-master/create-site.yml --extra-vars="which_host=localhost site_domain=$php74_name site_id=php74 php_install_version=7.4 server_runs_as=$non_admin_username server_runs_as_group=$non_admin_username" #-vvv
 
-	sudo ansible-playbook -i "localhost," -c local /tmp/infra-automation-master/create-site.yml --extra-vars="which_host=localhost site_domain=$php8_name site_id=php8 php_install_version=8.0 server_runs_as=$non_admin_username server_runs_as_group=$non_admin_username" #-vvv
+		sudo ansible-playbook -i "localhost," -c local /tmp/infra-automation-master/create-site.yml --extra-vars="which_host=localhost site_domain=$php8_name site_id=php8 php_install_version=8.0 server_runs_as=$non_admin_username server_runs_as_group=$non_admin_username" #-vvv
 
-	echo "Updating Launcher Icons"
-	gsettings set com.canonical.Unity.Launcher favorites "['application://nautilus.desktop', 'application://firefox.desktop', 'application://google-chrome.desktop', 'application://geany.desktop', 'application://gnome-terminal.desktop',  'application://skype.desktop', 'application://filezilla.desktop', 'application://virtualbox.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"
+		#echo "Updating Launcher Icons"
+		#gsettings set com.canonical.Unity.Launcher favorites "['application://nautilus.desktop', 'application://firefox.desktop', 'application://google-chrome.desktop', 'application://geany.desktop', 'application://gnome-terminal.desktop',  'application://skype.desktop', 'application://filezilla.desktop', 'application://virtualbox.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"
 	else
 		echo "Not running an Ubuntu distribution. ID=$ID, VERSION=$VERSION"
 	fi
